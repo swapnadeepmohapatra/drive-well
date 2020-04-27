daa52ca5c976cf8f0d25d56874c3ef8d192e5dc3
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-04-27 20:44:53 +0530

    Speed Activity

---
+++ app/src/main/java/com/swapnadeep/drivewell/SpeedActivity.java
@@ -0,0 +1,19 @@
+package com.swapnadeep.drivewell;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+public class SpeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speed);
+    }
+}
